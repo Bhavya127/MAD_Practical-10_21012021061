5b2a1584b3bf83bc5aa4549a23b59888f5c084a9
Bhavya127 <bhavya1273patel@gmail.com>
2023-10-30 15:27:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_practical_10_21012021061/PersonAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.mad_practical_10_21012021061
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.TextView
+
+class  PersonAdapter( context: Context,val personArray:ArrayList<Person>):ArrayAdapter<Person>(context,0,personArray)
+{
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val view=LayoutInflater.from(context).inflate(R.layout.person_item,parent,false)
+        val contact=getItem(position)
+
+        view.findViewById<TextView>(R.id.textName1).text = contact?.name
+        view.findViewById<TextView>(R.id.text_no).text = contact?.phoneNo
+        view.findViewById<TextView>(R.id.textemail).text = contact?.emailId
+        view.findViewById<TextView>(R.id.textaddress).text = contact?.address
+        val button1: Button = view.findViewById(R.id.location)
+        button1.setOnClickListener {
+            // Start the MapsActivity when button1 is clicked
+            val intent = Intent(context, MapsActivity::class.java)
+            context.startActivity(intent)
+        }
+
+        return view
+    }
+}
